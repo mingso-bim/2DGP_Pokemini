2c9400c24336e3fec11347de3f9b629258a6bb6b
mingso <kimdud0@naver.com>
2024-10-15 16:14:34 +0900

    게임 구조 작성

---
+++ main.py
@@ -0,0 +1,29 @@
+from pico2d import *
+from pico2d import close_canvas
+
+
+def Initialization():
+    pass
+
+
+def Update():
+    pass
+
+
+def Rendering():
+    pass
+
+
+def Handle_event():
+    pass
+
+
+
+Initialization()
+open_canvas()
+
+while True:
+    Update()
+    Rendering()
+
+close_canvas()
